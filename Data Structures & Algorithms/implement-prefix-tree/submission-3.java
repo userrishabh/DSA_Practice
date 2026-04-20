85645517385cc4faa0015309add0bb955fd5bb48
Rishabh Chaurasiya <43603041+userrishabh@users.noreply.github.com>
2026-04-21 01:49:30 +0530

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.java
@@ -0,0 +1,42 @@
+class PrefixTree {
+PrefixTree[] prefixTree;
+        boolean isLast;
+    public PrefixTree() {
+        prefixTree = new PrefixTree[26];
+        isLast = false;
+    }
+    
+    public void insert(String word) {
+        PrefixTree node = this;
+        for(int i=0; i<word.length(); i++)
+        {
+            if(node.prefixTree[word.charAt(i) - 'a'] == null)
+                node.prefixTree[word.charAt(i) - 'a'] = new PrefixTree();
+            node = node.prefixTree[word.charAt(i) - 'a'];
+        }
+        node.isLast = true;
+    }
+    
+    public boolean search(String word) {
+        PrefixTree node = this;
+        for(int i = 0; i<word.length(); i++)
+        {
+            if(node.prefixTree[word.charAt(i) - 'a'] == null) return false;
+            if(i == (word.length()-1) && node.prefixTree[word.charAt(i) - 'a'].isLast == false)
+            return false;
+            node = node.prefixTree[word.charAt(i) - 'a'];
+        }
+
+        return true;
+    }
+    
+    public boolean startsWith(String prefix) {
+        PrefixTree node = this;
+        for(int i=0; i<prefix.length(); i++)
+        {
+            if(node.prefixTree[prefix.charAt(i) - 'a'] == null) return false;
+            node = node.prefixTree[prefix.charAt(i) - 'a'];
+        }
+        return true;
+    }
+}
